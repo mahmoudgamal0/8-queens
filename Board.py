f7b3a8596a261e3b2ed319c6077ee780a49c4ee1
Abdelrahman Ibrahim <abdelrahman.ibrahim.009@gmail.com>
2019-12-10 16:05:42 +0200

    get best move

--- Board.py
+++ Board.py
@@ -29,7 +29,9 @@ class Board:
         algorithm.solve(self._config)
 
     def set_conflicting_pairs(self):
-        rows_of_queens = self.rows
+        self.conflicting_queens, self.conflicts_count = self.get_conflicting_queens(self.rows)
+
+    def get_conflicting_queens(self, rows_of_queens):
         conflicting_queens = set()
         conflicts_count = 0
         queens_count_in_row = [0 for i in range(0, 8)]
@@ -37,43 +39,47 @@ class Board:
         for i in range(0, 8):
             queens_count_in_row[rows_of_queens[i]] += 1
             queen_indexes[rows_of_queens[i]].append(i)
-
         for i in range(0, 8):
             if queens_count_in_row[i] > 1:
-                conflicts_count += queens_count_in_row[i]*(queens_count_in_row[i] - 1) / 2
+                conflicts_count += queens_count_in_row[i] * (queens_count_in_row[i] - 1) / 2
                 for index in queen_indexes[i]:
                     conflicting_queens.add(index)
-
         queen_indexes = [[] for i in range(0, 14)]
         queens_count_in_main_diag = [0 for i in range(0, 8)]
-
         for i in range(0, 8):
             s = rows_of_queens[i] + i
             queens_count_in_main_diag[s] += 1
             queen_indexes[s].append(i)
-
         for i in range(0, 14):
             if queens_count_in_main_diag[i] > 1:
-                conflicts_count += queens_count_in_main_diag[i]*(queens_count_in_main_diag[i] - 1) / 2
+                conflicts_count += queens_count_in_main_diag[i] * (queens_count_in_main_diag[i] - 1) / 2
                 for index in queen_indexes[i]:
                     conflicting_queens.add(index)
-
         queen_indexes = [[] for i in range(0, 14)]
         queens_count_in_sec_diag = [0 for i in range(0, 8)]
-
         for i in range(0, 8):
             s = (8 - rows_of_queens[i] + 1) + i
             queens_count_in_sec_diag[s] += 1
             queen_indexes[s].append(i)
-
         for i in range(0, 14):
             if queens_count_in_sec_diag[i] > 1:
-                conflicts_count += queens_count_in_sec_diag[i]*(queens_count_in_sec_diag[i] - 1) / 2
+                conflicts_count += queens_count_in_sec_diag[i] * (queens_count_in_sec_diag[i] - 1) / 2
                 for index in queen_indexes[i]:
                     conflicting_queens.add(index)
+        return conflicts_count, conflicting_queens
 
-        self.conflicting_queens = conflicting_queens
-        self.conflicts_count = conflicts_count
+    def get_best_position(self, queen):
+        current = self.rows
+        minimum = 99999
+        best = 0
+        for i in range(0, 8):
+            current[queen] = i
+            conflicts_count, _ = self.get_conflicting_queens(current)
+            if conflicts_count < minimum:
+                minimum = conflicts_count
+                best = i
+        return best, minimum
+
 #
 # def has_conflicts(self):
 #     for row in self._config:
