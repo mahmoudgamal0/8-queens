c877730b854eb32b4f9b7199990111ac1439ea0d
Mahmoud Gamal <mahmoud.gamal.15497@gmail.com>
2019-12-13 21:22:28 +0200

    Added gui and updated main

---
+++ gui.py
@@ -0,0 +1,45 @@
+import time
+import tkinter as tk
+
+
+def draw_grid(path):
+    root = tk.Tk()
+
+    c = tk.Canvas(root, height=1000, width=1000, bg='white')
+    c.pack(fill=tk.BOTH, expand=True)
+
+    di = 15
+    stx = 55
+    sty = 45
+
+    def draw(board):
+        for i in range(0, 8):
+            for j in range(0, 8):
+                if board[i][j]:
+                    x = (j * 125) + stx
+                    y = (i * 105) + sty
+                    c.create_oval(x, y, x + di, y + di, fill='red')
+
+    def create_grid(event):
+        w = c.winfo_width()
+        h = c.winfo_height()
+        c.delete('grid_line')
+
+        # Creates all vertical lines at intevals of 100
+        for i in range(0, w, 125):
+            c.create_line([(i, 0), (i, h)], tag='grid_line')
+
+        # Creates all horizontal lines at intevals of 100
+        for i in range(0, h, 105):
+            c.create_line([(0, i), (w, i)], tag='grid_line')
+
+    c.bind('<Configure>', create_grid)
+    for board in path:
+        c.delete('all')
+        draw(board)
+        create_grid(None)
+        root.update()
+        time.sleep(1)
+
+    root.quit()
+
--- main.py
+++ main.py
@@ -6,12 +6,19 @@ from GA import Genetic
 from HillClimb import HillClimb
 from K_beam import K_beam
 from fileIO import get_initial_configurations, write_new_configuration
+from gui import draw_grid
 from visualizer import visualize
 
 
 def main():
     board = get_initial_configurations()
+    run_hill(board)
+    run_csp(board)
+    run_genetic(board)
+    run_kbeam(board)
 
+
+def run_hill(board):
     # Hill Climbing
     time_hill = time.time()
     board_hill, explored_nodes_hill, path_hill = copy.deepcopy(board).solve(HillClimb())
@@ -19,29 +26,30 @@ def main():
     print_result('HillClimb', explored_nodes_hill, board_hill, time_hill)
     visualize(path_hill)
 
+    draw_grid(path_hill)
+
+
+def run_csp(board):
     # CSP WITHOUT BACKTRACKING
     time_csp = time.time()
     board_csp, expanded_nodes_csp, path_csp = copy.deepcopy(board).solve(CSP())
     time_csp = time.time() - time_csp
     print_result('CSP', expanded_nodes_csp, board_csp, time_csp)
     visualize(path_csp)
+    draw_grid(path_csp)
 
-    # GA
-    time_ga = time.time()
-    board_ga, expanded_nodes_ga = copy.deepcopy(board).solve(Genetic(100))
-    time_ga = time.time() - time_ga
-    print_result('GA', expanded_nodes_ga, board_ga, time_ga)
 
-    #K-beam
+def run_kbeam(board):
+    # K-beam
     k_beam = K_beam()
     best_time = 100
     best_time_pram = {}
     best_cost = 100
     best_cost_pram = {}
     found = False
-    for k in range (1,20):
+    for k in range(1, 20):
         time_k = time.time()
-        board_K_beam, expanded_nodes_k_beam, cost = k_beam.solve(board.config,k)
+        board_K_beam, expanded_nodes_k_beam, cost = k_beam.solve(board.config, k)
         time_k = time.time() - time_k
         if board_K_beam != None:
             found = True
@@ -49,7 +57,7 @@ def main():
             if best_time > time_k:
                 best_time = time_k
                 best_time_pram['k'] = k
-                best_time_pram['name'] = str(k)+"-beam"
+                best_time_pram['name'] = str(k) + "-beam"
                 best_time_pram['time'] = time_k
                 best_time_pram['cost'] = cost
                 best_time_pram['nodes'] = expanded_nodes_k_beam
@@ -57,16 +65,24 @@ def main():
             if best_cost > cost:
                 best_cost = cost
                 best_cost_pram['k'] = k
-                best_cost_pram['name'] = str(k)+"-beam"
+                best_cost_pram['name'] = str(k) + "-beam"
                 best_cost_pram['time'] = time_k
                 best_cost_pram['cost'] = cost
                 best_cost_pram['nodes'] = expanded_nodes_k_beam
                 best_cost_pram['board'] = board_K_beam
-    if(found):
-        print_result(best_time_pram['name'],best_time_pram['nodes'],
-            best_time_pram['board'],best_time_pram['time'])
-        print_result(best_cost_pram['name'],best_cost_pram['nodes'],
-            best_cost_pram['board'],best_cost_pram['time'])
+    if (found):
+        print_result(best_time_pram['name'], best_time_pram['nodes'],
+                     best_time_pram['board'], best_time_pram['time'])
+        print_result(best_cost_pram['name'], best_cost_pram['nodes'],
+                     best_cost_pram['board'], best_cost_pram['time'])
+
+
+def run_genetic(board):
+    # GA
+    time_ga = time.time()
+    board_ga, expanded_nodes_ga = copy.deepcopy(board).solve(Genetic(100))
+    time_ga = time.time() - time_ga
+    print_result('GA', expanded_nodes_ga, board_ga, time_ga)
 
 
 def print_result(method, expanded_nodes, board, exec_time):
@@ -74,5 +90,29 @@ def print_result(method, expanded_nodes, board, exec_time):
     write_new_configuration(board.config, method)
 
 
+# Run ALL
 if __name__ == '__main__':
     main()
+
+
+# Run Hill Only
+if __name__ == '__main__':
+    board = get_initial_configurations()
+    run_hill(board)
+
+
+# Run CSP Only
+if __name__ == '__main__':
+    board = get_initial_configurations()
+    run_csp(board)
+
+# Run KBeam Only
+if __name__ == '__main__':
+    board = get_initial_configurations()
+    run_kbeam(board)
+
+
+# Run Genetic Only
+if __name__ == '__main__':
+    board = get_initial_configurations()
+    run_genetic(board)
